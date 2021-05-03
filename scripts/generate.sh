305e4caa38bfa983333d1a61c3d961686e67403c
Dwaeck <dejan.waeckerlin@uzh.ch>
2021-05-03 18:51:17 +0200

    fixing a script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,6 +5,7 @@ base=$(realpath $scripts/..)
 
 models=$base/models
 data=$base/data
+examples=$base/examples
 tools=$base/tools
 samples=$base/samples
 
@@ -13,8 +14,8 @@ mkdir -p $samples
 num_threads=4
 device=""
 
-(cd $tools/pytorch-examples/word_language_model &&
-    CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python generate.py \
+(cd $examples/word_language_model &&
+    CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python3 generate.py --input "Some time ago, I used to believe in the history of our people. The history, we were told by our elders." \
         --data $data/new \
         --words 100 \
         --checkpoint $models/model_new.pt \
